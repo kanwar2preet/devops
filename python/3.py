8ebf0c568033b16b0a4bad7d4529cbf76e46214b
kanwar2preet <kanwar2preet@gmail.com>
2016-12-20 15:58:34 -0800

    Python3_basics.py

---
+++ python/3.py
@@ -0,0 +1,8 @@
+#Learning variables in a string
+x = "There are %d types of people" %10
+binary = "binary"
+donot  = "don't"
+y      = "Those who know %s and those who %s. " % (binary, donot)
+
+print(x)
+print(y)
