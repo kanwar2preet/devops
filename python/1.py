59e41551de388610b6bd7d18e61c22ea8abefe8d
kanwar2preet <kanwar2preet>
2016-12-20 00:11:00 -0800

    commit

--- python/1.py
+++ python/1.py
@@ -1,1 +1,7 @@
+# This is the start of the python script for learning python and act as a document backup
+#! python
+# measure the length of the strings
+a= ['vinty','gamesarethere','awesome']
+for x in a:
+    print x, len(x)
 
