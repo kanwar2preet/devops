5d6fcf5958da954d8b030e47a1211bc93efb324a
kanwar2preet <kanwar2preet@gmail.com>
2016-12-20 16:17:25 -0800

    Update 3.py

--- python/3.py
+++ python/3.py
@@ -1,4 +1,7 @@
 #Learning variables in a string
+print ("Lets start learning python choosing randowm topics out there to keep out interest & USING official documentation so that we donot loose track of what is going on")
+print("=============================================================")
+print("below example helps to understand complex strings which have variables in it")
 x = "There are %d types of people" %10
 binary = "binary"
 donot  = "don't"
@@ -6,3 +9,8 @@ y      = "Those who know %s and those who %s. " % (binary, donot)
 
 print(x)
 print(y)
+
+
+#print("I said: %r."  %(x) )
+#print("I also said: 's'." %(y) )
+ 
