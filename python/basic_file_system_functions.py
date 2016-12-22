208d379327de80188d67e3190fd41092267573ac
kanwar2preet <kanwar2preet>
2016-12-21 21:55:57 -0800

    continue

--- python/basic_file_system_functions.py
+++ python/basic_file_system_functions.py
@@ -2,6 +2,15 @@ import os
 os.getcwd() #getting pwd
 os.listdir() # ls 
 os.listdir("/var/log")   #ls /var/log
-os.mkdir("Test")
+os.mkdir("jest")
 os.listdir()
-os.chdir("Test")
+os.chdir("jest")
+os.getcwd()
+os.chdir("/home/kanwar")
+#os.walk can walk down directory paths
+for dirpath,dirnames,filenames in os.walk(os.getcwd(),topdown="true"):
+    print(dirpath,filenames)
+    
+    
+#continuing
+    
