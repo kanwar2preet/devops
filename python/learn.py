3b58ace4fdaa349b7d50529241003dbd996ab978
kanwar2preet <kanwar2preet@gmail.com>
2016-12-23 23:51:29 -0800

    Update learn.py

--- python/learn.py
+++ python/learn.py
@@ -6,7 +6,24 @@ a= ['vinty','gamesarethere','awesome']
 for x in a:
     print x, len(x)
 
+##############################################################
+##Learning default Agruments in funtions
+
+def function(message,times=1):
+    print(message * times)
+
+function('hello')
+function('hello', 8)
+##############################################
+####Keyword Arguments
+def key_arguments(a=4, b=6, c=9):
+    print('a is',a, 'b is', b, 'c is',c)
+
+key_arguments(2,4)
+key_arguments(1,3)
+key_arguments(1,1,1)
+
     
     
     
-    
+
