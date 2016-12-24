73d42502f05deb619af08324873d82d6e50d7c95
kanwar2preet <kanwar2preet@gmail.com>
2016-12-24 00:01:22 -0800

    Update learn.py

--- python/learn.py
+++ python/learn.py
@@ -35,6 +35,28 @@ def maximum(x,y):
 
 print(maximum(2,3))
     
+
+######################################################
+##############Doc strings
+def print_max(x,y):
+    '''Prints the maximum of two numbers.
+
+    The two values must be integers.'''
+    x=int(x)
+    y=int(y)
+
+    if x > y:
+        print('x is greater')
+    else:
+        print('y is greater')
+
+print_max(3,5)
+print(print_max.__doc__)
+
+######################################################
+
+    
+    
     
     
     
