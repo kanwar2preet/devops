f746f73bab39d8e2591eedb2c8b466fd24ee34c5
kanwar2preet <kanwar2preet@gmail.com>
2016-12-23 23:43:10 -0800

    Update test.py

--- python/test.py
+++ python/test.py
@@ -5,5 +5,21 @@ print("Concatenation of strings")
 print('Alice' + 'bob')
 
 
+##Learning default Agruments in funtions
 
+def function(message,times=1):
+    print(message * times)
+
+function('hello')
+function('hello', 8)
+##############################################
+####Keyword Arguments
+def key_arguments(a=4, b=6, c=9):
+    print('a is',a, 'b is', b, 'c is',c)
+
+key_arguments(2,4)
+key_arguments(1,3)
+key_arguments(1,1,1)
+
+##############################################
 
