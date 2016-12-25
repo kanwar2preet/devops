a1eb873ab50b40f9534ec5d76ed35d4821a72b63
kanwar2preet <kanwar2preet@gmail.com>
2016-12-24 22:07:42 -0800

    Update webcrawler.py

--- python/webcrawler.py
+++ python/webcrawler.py
@@ -3,13 +3,9 @@ import urllib.request
 
 def download_web_image(url):
     name = random.randrange(1,1000)
-    full_name = str(name) + ".jpeg"
-    urllib.request.urlretrieve(url,fullname)
+    full_name = str(name) + ".jpg"
+    urllib.request.urlretrieve(url, full_name)
 
-download_web_image("https://thenewboston.com/photos/users/8299/resized/d54bcecc3d46d82b31dffbb00a5dfdc0.jpg")	
-
-
-
-
+download_web_image("https://thenewboston.com/photos/users/8299/resized/d54bcecc3d46d82b31dffbb00a5dfdc0.jpg")
 
 
