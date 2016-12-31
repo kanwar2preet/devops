235917cf26b3030354faec5d46efcf7ff1016b31
kanwar2preet <kanwar2preet@gmail.com>
2016-12-31 10:20:03 -0800

    Update regularExpression.py

--- python/regularExpression.py
+++ python/regularExpression.py
@@ -12,3 +12,29 @@ def Find(pattern,text):
 # regular expression funtion defined above
 
 Find('ig', 'called ig' )
+
+
+# . (dot) for any character
+
+Find ('..g','called piiii g much better: xyzg')
+# searches from left to right and immediately stops when find the first one
+
+
+#see below code if you want to search for a . character, basically put a \ to reduce specialness of dot character
+
+Find (r'c\.','c.lled piiii g much better: xyzg')
+
+#in above line r' tells to find something as it is and treats basically everything as raw
+
+
+#to find a word character use \w like below
+
+Find (r':\w\w\w','called piiii :cat much better: xyzg')
+
+#to find the digits use \d like below
+Find (r':\d\d\d','ca lled piiii g :123 better: xyzg')
+
+#to find the whitespace use the /s
+
+Find (r'\d\s\d\s\d\s','called piiii g 1 2 3 better: xyzg')
+        
