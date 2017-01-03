266cdf280edb31884c6d185cf3f71958f45fc798
kanwar2preet <kanwar2preet@gmail.com>
2017-01-02 23:30:05 -0800

    Update loops.py

--- python/loops.py
+++ python/loops.py
@@ -8,3 +8,15 @@ for n in [2,4,5,6,7,8,]:
 #A2 : for loop continued:
 for n in range(8):
     print("numbers are",n)
+
+
+#A3: for with range    
+for n in range(3,5):
+    print("ranges are",n)
+
+#B1 : Whille loop
+count = 0
+while count <=5:
+    print("going from 0 to 5,number is",count)
+    count += 1
+    
