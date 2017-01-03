e773d1f6c6f0e4ce569bbf32e7ed8261aad5cb10
kanwar2preet <kanwar2preet@gmail.com>
2017-01-02 23:29:00 -0800

    Create loops.py

---
+++ python/loops.py
@@ -0,0 +1,10 @@
+#Looping in python
+
+#A:Print numbers in a list (using for loop)
+for n in [2,4,5,6,7,8,]:
+    print(n)
+    
+
+#A2 : for loop continued:
+for n in range(8):
+    print("numbers are",n)
