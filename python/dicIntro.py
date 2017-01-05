845f7c5bc76717c39f77fde51ffc9218ec69ae5f
kanwar2preet <kanwar2preet@gmail.com>
2017-01-04 23:55:50 -0800

    Create dicIntro.py

---
+++ python/dicIntro.py
@@ -0,0 +1,18 @@
+#! /usr/local/bin/python3
+print("this script will show the use of dict in python")
+
+dict = {'game1':'red5','game2':'mw','game3':'csgo','game4':'pop'}
+
+print(dict['game1'])
+
+print('Updating a dictionary')
+
+print("value of game one",dict['game1'])
+
+print("Value of game2",dict['game2'])
+
+dict['game2'] = "not playing" #updating dict value
+
+
+print("value of game2","dict['game2'])
+
