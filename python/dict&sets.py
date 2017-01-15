354b58669094d69317b75f24df99f4603f65cd04
kanwar2preet <kanwar2preet@gmail.com>
2017-01-14 20:36:54 -0800

    Create dict&sets.py

---
+++ python/dict&sets.py
@@ -0,0 +1,9 @@
+print("Learning_tuples & sets ")
+print("Creating tuple")
+videoData = tuple(["Introduction",25,"anotherstr"])
+print(videoData)
+videoTitle, totalVideotime, junkname = videoData
+print(videoTitle)
+print(totalVideotime)
+print(junkname)
+##########################################
